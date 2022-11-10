205493c7faced118104d076c3051e19505d16612
Andriy Pavlykivskyi <greenke74@gmail.com>
2022-11-10 23:35:33 +0200

    changed composition architechture, add form for counter

--- src/components/BlocksComposition/Block.jsx
+++ src/components/BlocksComposition/Block.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, lazy, Suspense } from 'react'
+import { useForm } from 'react-hook-form';
 import { Box, ButtonGroup, Button, Typography, styled, IconButton, Tooltip, Fade } from '@mui/material'
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import MuiAccordion from '@mui/material/Accordion';
@@ -14,7 +15,9 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import Swal from 'sweetalert2';
 import '../../styles/swal.css';
 import './block.css';
-import { SettingsBlock } from '../blocks';
+import { getBlock } from '../../services/blocks-api-service';
+import { blocks } from '../blocks';
+import { Spinner } from '../common/StyledComponents';
 
 const Accordion = styled((props) => (
 	<MuiAccordion disableGutters elevation={0} {...props} />
@@ -36,7 +39,7 @@ const AccordionSummary = styled((props) => (
 	/>
 ))(({ theme }) => ({
 	flexGrow: 1,
-	minHeight: '100% !important',
+	height: '56px',
 	paddingTop: 6,
 	paddingBottom: 6,
 	backgroundColor:
@@ -52,8 +55,23 @@ const AccordionSummary = styled((props) => (
 	},
 }));
 
-const Block = ({ block, idx, remove, fields, move, onSubmit,isPublished, setIsPublished }) => {
+const Block = ({ block, idx, remove, fields, move, onSubmit }) => {
 	const [expanded, setExpanded] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [blockData, setBlockData] = useState({});
+	const [Element, setElement] = useState(null);
+
+	const form = useForm({
+		defaultValues: {
+			is_published: true,
+			data: null,
+			completed_projects: null,
+			offers: null,
+			services: null
+		}
+	})
+	const is_published = form.watch('is_published');
+
 	const onDelete = () => {
 		Swal.fire({
 			title: 'Видалити блок',
@@ -70,71 +88,87 @@ const Block = ({ block, idx, remove, fields, move, onSubmit,isPublished, setIsPu
 		})
 	}
 
-	return (
-		<Accordion expanded={expanded} >
-			<Box
-				display='flex'
-				width='100%'
-				justifyContent='space-between'
-				alignItems='center'
-				flexWrap='nowrap'
-			>
-				<AccordionSummary
-					onClick={() => setExpanded(prev => !prev)}
-				>
-					<Typography
-						component="h3"
-						fontSize='14px'
-						flexGrow={1}
-						flexShrink={0}
-						lineHeight='20px'
-					>
-						{block.type}
-					</Typography>
-				</AccordionSummary>
-				<Box display='flex' flexWrap='nowrap' style={{ gap: '10px' }} alignItems='center' padding='10px' bgcolor='#f7f7f7'>
-					<Tooltip title={isPublished ? 'Опубліковано' : 'Приховано'}>
-						<IconButton
-							size='small'
-							onClick={() =>  setIsPublished(idx, !Boolean(isPublished))}
-						>
-							{isPublished
-								? <VisibilityIcon style={{ fontSize: '20px', color: 'var(--theme-color)' }} />
-								: <VisibilityOffIcon style={{ fontSize: '20px', }} />}
-						</IconButton>
-					</Tooltip>
-					<ButtonGroup>
-						<Button
-							disableRipple={true}
-							onClick={() => move(idx, idx + 1)}
-							disabled={idx + 1 == fields.length}
-						><ArrowCircleDownIcon />
-						</Button>
-						<Button
-							disableRipple={true}
-							onClick={() => move(idx, idx - 1)}
-							disabled={idx == 0}
+	useEffect(() => {
+		let mounted = true;
+		mounted && setLoading(true);
 
-						><ArrowCircleUpIcon />
-						</Button>
-						<Button
-							color='error'
-							onClick={onDelete}
-						><HighlightOffIcon />
-						</Button>
-					</ButtonGroup>
+		getBlock(block).then(data => {
+			mounted && setBlockData(data);
+			mounted && setElement(lazy(blocks.find(b => b.type === data?.type ?? null).element))
+			const { is_published, data: bdata, completed_projects, offers, services } = data;
+			mounted && form.reset({ is_published, data: bdata, completed_projects, offers, services })
+		})
+			.catch(e => console.error(e))
+			.finally(() => {
+				mounted && setLoading(false);
+			})
+		return () => mounted = false;
+	}, [block])
+
+	return loading
+		? (<div style={{ width: '100%', height: 58, backgroundColor: '#f7f7f7', borderRadius: 4 }}></div>)
+		: (
+			<Accordion expanded={expanded} >
+				<Box
+					display='flex'
+					width='100%'
+					justifyContent='space-between'
+					alignItems='center'
+					flexWrap='nowrap'
+				>
+					<AccordionSummary
+						onClick={() => setExpanded(prev => !prev)}
+					>
+						<Typography
+							component="h3"
+							fontSize='14px'
+							flexGrow={1}
+							flexShrink={0}
+							lineHeight='20px'
+						>
+						</Typography>
+					</AccordionSummary>
+					<Box display='flex' flexWrap='nowrap' style={{ gap: '10px' }} alignItems='center' padding='10px' bgcolor='#f7f7f7'>
+						<Tooltip title={is_published ? 'Опубліковано' : 'Приховано'}>
+							<IconButton
+								size='small'
+								onClick={() => { form.setValue('is_published', !is_published) }}
+							>
+								{is_published
+									? <VisibilityIcon style={{ fontSize: '20px', color: 'var(--theme-color)' }} />
+									: <VisibilityOffIcon style={{ fontSize: '20px', }} />}
+							</IconButton>
+						</Tooltip>
+						<ButtonGroup>
+							<Button
+								disableRipple={true}
+								onClick={() => move(idx, idx + 1)}
+								disabled={idx + 1 == fields.length}
+							><ArrowCircleDownIcon />
+							</Button>
+							<Button
+								disableRipple={true}
+								onClick={() => move(idx, idx - 1)}
+								disabled={idx == 0}
+							><ArrowCircleUpIcon />
+							</Button>
+							<Button
+								color='error'
+								onClick={onDelete}
+							><HighlightOffIcon />
+							</Button>
+						</ButtonGroup>
+					</Box>
 				</Box>
-			</Box>
-			<AccordionDetails>
-				<Box className='block-settings' marginY={2}>
-					<SettingsBlock
-						type={block.type}
-						onSubmit={onSubmit}
-					/>
-				</Box>
-			</AccordionDetails>
-		</Accordion >
-	)
+				{blockData && <AccordionDetails>
+					<Box className='block-settings' marginY={2}>
+						{Element ? (
+							<Element form={form} />
+						) : null}
+					</Box>
+				</AccordionDetails>}
+			</Accordion >
+		)
 }
 
 export default Block
