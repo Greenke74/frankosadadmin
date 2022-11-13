251e600276471d1e2174bf9fe4413b1a5f0fe9ab
rost <rsharamailuk13@gmail.com>
2022-11-13 23:05:21 +0200

    deleteBlock and udpateBlock methods

--- src/services/blocks-api-service.js
+++ src/services/blocks-api-service.js
@@ -25,7 +25,41 @@ export const getBlock = (block_id) => new Promise((resolve, reject) => {
 export const insertBlock = (data) => new Promise((resolve, reject) => {
     try {
         supabase
-        .rpc('insert_block', {data})
+        .rpc('insert_block', data)
+            .then(response => {
+                if(response.error){
+                    reject(response.error.message)
+                }
+                resolve(response)       
+            })
+            .catch(error => reject(error))
+    } catch (e) {
+        reject(e)
+    }
+})
+
+export const deleteBlock = (block_id) => new Promise((resolve, reject) => {
+    try {
+        supabase
+        .from('blocks')
+        .delete()
+        .eq("id", block_id)
+            .then(response => {
+                if(response.error) {
+                    reject(response.error.message)
+                }
+                resolve(response)
+            })
+            .catch(error => reject(error))
+    } catch (e) {
+        reject(e)
+    }
+})
+
+export const updateBlock = (data) => new Promise((resolve, reject) => {
+    try {
+        supabase
+        .rpc('update_block', data)
             .then(response => {
                 if(response.error){
                     reject(response.error.message)
