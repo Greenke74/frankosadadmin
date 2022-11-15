73da6bddeeeddd1a4139a645f16434b0ba2b5bfa
rost <rsharamailuk13@gmail.com>
2022-11-15 21:44:57 +0200

    insert completed project

---
+++ src/services/completed_project-service.js
@@ -0,0 +1,15 @@
+import { supabase } from "../supabase/supabaseClient.js";
+
+export const insertCompletedProject = (data) => new Promise((resolve, reject) => {
+    try {
+        supabase.rpc('insert_completed_project', data)
+        .then(response => {
+            if(response.error) {
+                reject(response.error.message)
+            }
+        })
+        .catch(error => reject(error))
+    } catch (e) {
+        reject(e)
+    }
+})
