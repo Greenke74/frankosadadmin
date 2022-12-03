b8eb516ae47ff49942194da2d68d7547156bd75d
rost <rsharamailuk13@gmail.com>
2022-12-03 21:48:18 +0200

    update image upload method using uuid

--- src/services/storage-service.js
+++ src/services/storage-service.js
@@ -1,17 +1,17 @@
 import { supabase } from "../supabase/supabaseClient";
-
+import { v1 as uuidv1 } from 'uuid'
 
 export const uploadImage = (file) => new Promise((resolve, reject) => {
 	try {
 		supabase.storage
 			.from('images')
-			.upload("public/" + file?.name, file, { upsert: true })
+			.upload("public/" + uuidv1() + file?.name, file, { upsert: true })
 			.then(response => {
 				if (response.error) {
 					reject(response.error.message)
 				}
 				const { data: { path } } = response
-				resolve(supabase.storage.from('images').getPublicUrl(path).data.publicUrl)
+				resolve(path)
 			})
 			.catch(error => reject(error))
 	} catch (e) {
